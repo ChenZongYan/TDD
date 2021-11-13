5c42bfd4983b4d077fb672beafe75160265cd129
陳宗彥 <zongyan.chen@egk.com.tw>
2021-11-13 22:45:53 +0800

    移除測試code

--- TDD/TDD_Budget/BudgetService.cs
+++ TDD/TDD_Budget/BudgetService.cs
@@ -33,19 +33,14 @@ namespace UnitTest
             var startDateAndEndDateIsSameYearMonth = start.Year == end.Year && start.Month == end.Month;
             
             var startTotalDays = startDateAndEndDateIsSameYearMonth ? (end.Day - start.Day + 1) : (daysInMonthByStartDate - start.Day + 1);
-            var endTotalDays = startDateAndEndDateIsSameYearMonth ? (end.Day - start.Day + 1) : (daysInMonthByEndDate - end.Day + 1);
-
-            Console.WriteLine("Start Total :"+startTotalDays);
-            Console.WriteLine("End Total :"+endTotalDays);
+            var endTotalDays = startDateAndEndDateIsSameYearMonth ? (end.Day - start.Day + 1) : (daysInMonthByEndDate - end.Day + 1);;
 
              return budgets.Where(
                 x =>
                 {
                     var yearMonth = DateTime.ParseExact(x.YearMonth, "yyyyMM", CultureInfo.InvariantCulture);
 
-                    var totalMonth = yearMonth.Year * 12 + yearMonth.Month;
-
-                    Console.WriteLine($"{totalMonth} / {totalQueryStartMonth} / {totalQueryEndMonth}");
+                    var totalMonth = yearMonth.Year * 12 + yearMonth.Month;;
 
                     return totalQueryStartMonth <= totalMonth && totalMonth <= totalQueryEndMonth;
                 }
@@ -59,25 +54,17 @@ namespace UnitTest
 
                 if (isMustCalculateAvgAmountByStartDate)
                 {
-                    Console.WriteLine($"start {startTotalDays} / {daysInMonthByStartDate}");
                     return (item.Amount / daysInMonthByStartDate) * startTotalDays;
                 }
                 else if (isMustCalculateAvgAmountByEndDate)
                 {
-                    Console.WriteLine($"end {endTotalDays} / {daysInMonthByEndDate}");
                     return (item.Amount / daysInMonthByEndDate) * endTotalDays;
                 }
                 else
                 {
-                    Console.WriteLine("All");
                     return (decimal)item.Amount;
                 }
             }).Sum();
-        }
-        public IEnumerable<DateTime> EachDay(DateTime from, DateTime thru)
-        {
-            for(var day = from.Date; day.Date <= thru.Date; day = day.AddDays(1))
-                yield return day;
         }
     }
 }
